a0cf02d5ec08951cc6be662271b766b8140da31a
TejasAnand <anandtejas455@gmail.com>
2023-08-14 03:09:42 +0530

    ranking-page

--- src/app/game/[gameName]/page.tsx
+++ src/app/game/[gameName]/page.tsx
@@ -1,4 +1,5 @@
 import { Youtube } from "@/components/youtube";
+import Image from "next/image";
 
 interface GameProps {
   params: {
@@ -6,19 +7,88 @@ interface GameProps {
   };
 }
 
-<<<<<<< HEAD
 export default function Game(props: GameProps) {
   return (
-    <div className="embed-style">
+    <div>
+      <div className="embed-style">
 
-      <Youtube id="dQw4w9WgXcQ" title="Title" />
+        <Youtube id="dQw4w9WgXcQ" title="Title" />
+      </div>
 
+      <ul className='ranks-wrapper'>
+        <li className="rank">
+          <Image
+            src="/valorantranks/iron.png"
+            alt="logo"
+            width={60}
+            height={60}
+          />
+        </li>
+        <li className="rank">
+          <Image
+            src="/valorantranks/bronze.png"
+            alt="logo"
+            width={60}
+            height={60}
+          />
+        </li>
+        <li className="rank">
+          <Image
+            src="/valorantranks/silver.png"
+            alt="logo"
+            width={60}
+            height={60}
+          />
+        </li>
+        <li className="rank">
+          <Image
+            src="/valorantranks/gold.png"
+            alt="logo"
+            width={60}
+            height={60}
+          />
+        </li>
+        <li className="rank">
+          <Image
+            src="/valorantranks/plat.png"
+            alt="logo"
+            width={60}
+            height={60}
+          />
+        </li>
+        <li className="rank">
+          <Image
+            src="/valorantranks/diamond.png"
+            alt="logo"
+            width={60}
+            height={60}
+          />
+        </li>
+        <li className="rank">
+          <Image
+            src="/valorantranks/ascendant.png"
+            alt="logo"
+            width={60}
+            height={60}
+          />
+        </li>
+        <li className="rank">
+          <Image
+            src="/valorantranks/immortal.png"
+            alt="logo"
+            width={60}
+            height={60}
+          />
+        </li>
+        <li className="rank">
+          <Image
+            src="/valorantranks/radiant.png"
+            alt="logo"
+            width={60}
+            height={60}
+          />
+        </li>
+      </ul>
     </div>
-
-
   )
-=======
-export default function Game(params: GameProps) {
-  return <Youtube id="dQw4w9WgXcQ" title="Title" />;
->>>>>>> fd652e1 (Add sidebar+header+themes)
 }
