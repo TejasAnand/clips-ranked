307ddd5f2be0be00f3ddb7f3e74527cce7b51366
TejasAnand <anandtejas455@gmail.com>
2023-08-13 23:47:05 +0530

    styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Icon } from 'next/dist/lib/metadata/types/metadata-types';
+
+
 
 
 // const sidebarItems = [
@@ -25,66 +26,68 @@ import { Icon } from 'next/dist/lib/metadata/types/metadata-types';
 //         img: <Image src="/forntite.jpg" alt="logo" width={200} height={200} className="sidebar_logo" />,
 //     },];
 export default function Sidebar() {
-    return <div>
+    return (
+        <div className='sidebar_wrapper'>
 
-        <aside className='sidebar'>
-            <div className='sidebar_top'>
-                <Image
-                    src="/head.svg"
-                    alt="logo"
-                    width={10}
-                    height={10}
-                    className="sidebar_logo"
-                />
+            <aside className='sidebar'>
+                <div className='sidebar_top'>
+                    <Image
+                        src="/head.svg"
+                        alt="logo"
+                        width={10}
+                        height={10}
+                        className="sidebar_logo"
+                    />
 
-            </div>
-            <ul className="sidebar_list">
+                </div>
+                <ul className="sidebar_list">
 
-                <li className="sidebar_item">
-                    <span className="sidebar_icon">
-                        <Image
-                            src="/valorant2.svg"
-                            alt="logo"
-                            width={60}
-                            height={60}
-                        />
+                    <li className="sidebar_item">
+                        <span className="sidebar_icon">
+                            <Image
+                                src="/valorant2.svg"
+                                alt="logo"
+                                width={60}
+                                height={60}
+                            />
 
-                    </span>
-                </li>
-                <li className="sidebar_item">
-                    <span className="sidebar_icon">
-                        <Image
-                            src="/counterstrike.svg"
-                            alt="logo"
-                            width={60}
-                            height={60}
-                        />
+                        </span>
+                    </li>
+                    <li className="sidebar_item">
+                        <span className="sidebar_icon">
+                            <Image
+                                src="/counterstrike.svg"
+                                alt="logo"
+                                width={60}
+                                height={60}
+                            />
 
-                    </span>
-                </li>
-                <li className="sidebar_item">
-                    <span className="sidebar_icon">
-                        <Image
-                            src="/leagueoflegends.svg"
-                            alt="logo"
-                            width={60}
-                            height={60}
-                        />
+                        </span>
+                    </li>
+                    <li className="sidebar_item">
+                        <span className="sidebar_icon">
+                            <Image
+                                src="/leagueoflegends.svg"
+                                alt="logo"
+                                width={60}
+                                height={60}
+                            />
 
-                    </span>
-                </li>
-                <li className="sidebar_item">
-                    <span className="sidebar_icon">
-                        <Image
-                            src="/rl.svg"
-                            alt="logo"
-                            width={60}
-                            height={60}
-                        />
+                        </span>
+                    </li>
+                    <li className="sidebar_item">
+                        <span className="sidebar_icon">
+                            <Image
+                                src="/rl.svg"
+                                alt="logo"
+                                width={60}
+                                height={60}
+                            />
 
-                    </span>
-                </li>
-            </ul>
-        </aside>
-    </div>;
+                        </span>
+                    </li>
+                </ul>
+            </aside>
+        </div>
+    )
 }
