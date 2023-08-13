3b33f8bc624e0d21c15793b722947050c0d3b4ed
TejasAnand <anandtejas455@gmail.com>
2023-08-13 20:44:10 +0530

    sidebar items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,29 @@
-
+import Link from 'next/link';
 import Image from 'next/image';
+
+
+const sidebarItems = [
+    {
+        name: 'Valorant',
+        href: "/valorant",
+        img: <Image src="/valorant.jpg" alt="logo" width={200} height={200} className="sidebar_logo" />,
+    },
+    {
+        name: 'League of Legends',
+        href: "/leagueoflegends",
+        img: <Image src="/leagueoflegends.jpg" alt="logo" width={200} height={200} className="sidebar_logo" />,
+    },
+    {
+        name: 'Counter Strike',
+        href: "/counterstrike",
+        img: <Image src="/counterstrike.jpg" alt="logo" width={200} height={200} className="sidebar_logo" />,
+    },
+    {
+        name: 'Fortnite',
+        href: "/fortnite",
+        img: <Image src="/forntite.jpg" alt="logo" width={200} height={200} className="sidebar_logo" />,
+    },];
 export default function Sidebar() {
     return <div>
-
-        <aside className='sidebar'>
-            <div>
-                <Image
-                    src="/valorant.jpg"
-                    alt="logo"
-                    width={200}
-                    height={200}
-                />
-            </div>
-        </aside>
-    </div>;
+    </div>
 }
